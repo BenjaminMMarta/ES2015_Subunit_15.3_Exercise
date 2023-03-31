0df36267d6e9059476a2c49a9b2253a60567802b
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-03-31 14:44:48 -0700

    Added ES2015 Subunit 15.3 Exercise in JavaScript file.

---
+++ app.js
@@ -0,0 +1,9 @@
+//--------------------Arrow Functions Exercise--------------------//
+
+//ES5 Map Callback//
+const double = arr => arr.map(val => val * 2);
+
+//Refactor the following function to use arrow functions//
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0)
+
+//--------------------End--------------------//
